ec711e9c48bf92d3f4c7337262473615e86600cb
tarekul42 <tarekulrifat205@gmail.com>
2023-09-02 14:02:02 -0700

    working at responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,11 +4,11 @@ import { NavLink, } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>
+        <div className='navigation'>
             
             <NavLink to='/Watch'>Watch</NavLink>
             <NavLink to='/Alarm' >Alarm</NavLink>
-            <NavLink to='StopWatch'>Stop Watch</NavLink>
+            <NavLink to='StopWatch'>Stopwatch</NavLink>
             <NavLink to='Timer'>Timer</NavLink>
         </div>
     );
