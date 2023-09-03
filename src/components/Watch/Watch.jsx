91ce22a70b5b0e1d7cf5938c700f5cd28f0bc67a
tarekul42 <tarekulrifat205@gmail.com>
2023-09-03 02:07:34 -0700

    watch section almost finished

--- src/components/Watch/Watch.jsx
+++ src/components/Watch/Watch.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Watch.css'
 
 const Watch = () => {
-    let hrs = document.getElementById('hrs')
-    let min = document.getElementById('min')
-    let sec = document.getElementById('sec')
+    
+    let time = new Date().toLocaleTimeString();
+
+    const [currentTime, setCurrentTime] = useState(time);
+
     setInterval(() => {
-        hrs.innerText = new Date().getHours();
-        min.innerText = new Date().getMinutes();
-        sec.innerText = new Date().getSeconds();
+        time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+        
     }, 1000);
+
+
+
     return (
         <div className='flex justify-center items-center watch-container'>
             
             <div className='clock'>
-                <span id='hrs'></span>
-                <span >:</span>
-                <span id='min'></span>
-                <span>:</span>
-                <span id='sec'></span>
+                <span>{currentTime}</span>
             </div>
         </div>
     );
