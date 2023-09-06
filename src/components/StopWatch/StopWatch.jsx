0a3cdad73bcec553f8b81b0852fdb2ec2ce2f63a
tarekul42 <tarekulrifat205@gmail.com>
2023-09-06 15:25:11 -0700

    stopwatch almost done

--- src/components/StopWatch/StopWatch.jsx
+++ src/components/StopWatch/StopWatch.jsx
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import './StopWatch.css'
 
 const StopWatch = () => {
-    const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+    const [time, setTime] = useState({s:0, m:0, h:0});
     const [interv, setInterv] = useState();
     const [status, setStatus] = useState(0);
 
     const start = () => {
         run();
         setStatus(1);
-        setInterv(setInterval(run, 10));
+        setInterv(setInterval(run, 1000));
     };
 
-    var updatems = time.ms, updateS = time.s, updateM = time.m ,updateH = time.h;
+    var updateS = time.s, updateM = time.m ,updateH = time.h;
 
     const run = () =>{
         if(updateM === 60){
@@ -23,12 +23,8 @@ const StopWatch = () => {
             updateS = 0;
             updateM = updateM + 1;
         }
-        if(updatems === 100){
-            updatems = 0;
-            updateS = updateS + 1;
-        }
-        updatems = updatems + 1;
-        return setTime({ms:updatems, s:updateS, m:updateM, h:updateH});
+        updateS = updateS + 1;
+        return setTime({s:updateS, m:updateM, h:updateH});
 
     }
     const stop = () => {
@@ -38,50 +34,41 @@ const StopWatch = () => {
     const reset = () => {
         clearInterval(interv);
         setStatus(0);
-        setTime({ms:0, s:0, m:0, h:0});
+        setTime({s:0, m:0, h:0});
     };
     const resume = () => start();
 
-    const h = () =>{
-        if(time.h === 0){
-            return '';
-        }else{
-            return <span>{(time.h >= 10) ? time.h : "0"+time.h}</span>
-        }
-    }
-
     return (
-        <div className='main-section'>
-            <div className='clock-holder'>
-                <div className='stopwatch'>
-                {h()} &nbsp; : &nbsp;
-                <span>{(time.m >= 10) ? time.m : "0"+time.m}</span> &nbsp; : &nbsp;
-                <span>{(time.s >= 10) ? time.s : "0"+time.s}</span> &nbsp; : &nbsp;
-                <span>{(time.ms >= 10) ? time.ms : "0"+time.ms}</span>&nbsp;  &nbsp;
+        <div className='flex justify-center items-center'>
+            <div className='stopwatch text-center'>
+                <div className='flex items-center'>
+                <span className='span'>{(time.h >= 10) ? time.h : "0"+time.h}</span> <span className='dot-span'>&nbsp; : &nbsp;</span>  
+                <span className='span'>{(time.m >= 10) ? time.m : "0"+time.m}</span> <span className='dot-span'>&nbsp; : &nbsp;</span>  
+                <span className='span'>{(time.s >= 10) ? time.s : "0"+time.s}</span>
+                </div>
                 {/* buttons are here */}
                 <div>
                     {
                         (status === 0)?
-                        <button className='start-btn' onClick={start}>Start</button>
+                        <button onClick={start}>Start</button>
                         :
                         ""
                     }
                     {
                         (status === 1)?
                         <div>
-                            <button className='stop-btn' onClick={stop}>Stop</button>
-                            <button className='reset-btn' onClick={reset}>Reset</button>
+                            <button onClick={stop}>Stop</button>
+                            <button onClick={reset}>Reset</button>
                         </div> : ""
                     }
                     {
                         (status === 2)?
                         <div>
-                            <button className='stop-btn' onClick={resume}>Resume</button>
-                            <button className='reset-btn' onClick={reset}>Reset</button>
+                            <button onClick={resume}>Resume</button>
+                            <button onClick={reset}>Reset</button>
                         </div> : ""
                     }
 
-                </div>
                 </div>
             </div>
         </div>
